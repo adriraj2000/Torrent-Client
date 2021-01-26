d56c35b5164d5e51eba76b1b57a2a57e729e340b
adriraj2000 <adrirajc@gmail.com>
2021-01-26 19:51:19 +0530

    implemented peer handlers

--- peer.py
+++ peer.py
@@ -87,27 +87,93 @@ class Peer(object):
         logging.debug('handle_interested - %s' % self.ip)
         self.state['peer_interested'] = True
 
-        pass
+        if self.am_choking():
+            unchoke = message.UnChoke().to_bytes()
+            self.send_to_peer(unchoke)    
 
     def handle_not_interested(self):
         logging.debug('handle_not_interested - %s' % self.ip)
         self.state['peer_interested'] = False
     
     def handle_have(self,have):
-        pass
+        logging.debug('handle_have - ip: %s - piece: %s' % (self.ip, have.piece_index))
+        self.bit_field[have.piece_index] = True
+
+        if self.is_choking() and not self.state['am_interested']:
+            interested = message.Interested().to_bytes()
+            self.send_to_peer(interested)
+            self.state['am_interested'] = True
 
     def handle_bitfield(self,bitfield):
-        pass
+        logging.debug('handle_bitfield - %s - %s' % (self.ip, bitfield.bitfield))
+        self.bit_field = bitfield.bitfield
+
+        if self.is_choking() and not self.state['am_interested']:
+            interested = message.Interested().to_bytes()
+            self.send_to_peer(interested)
+            self.state['am_interested'] = True
 
     def handle_piece(self,message):
-        pass
+        pub.sendMessage('PiecesManager.Piece', piece=(message.piece_index, message.block_offset, message.block))
 
-    def handle_request(self,request):
-        pass
-
+    def handle_request(self, request):
+        """
+        :type request: message.Request
+        """
+        logging.debug('handle_request - %s' % self.ip)
+        if self.is_interested() and self.is_unchoked():
+            pub.sendMessage('PiecesManager.PeerRequestsPiece', request=request, peer=self)
+        
     def handle_cancel(self):
         logging.debug('handle_cancel - %s' % self.ip)
 
     def handle_port_request(self):
         logging.debug('handle_port_request - %s' % self.ip)
+
+    def _handle_handshake(self):
+        try:
+            handshake_message = message.Handshake.from_bytes(self.read_buffer)
+            self.has_handshaked = True
+            self.read_buffer = self.read_buffer[handshake_message.total_length:]
+            logging.debug('handle_handshake - %s' % self.ip)
+            return True
+
+        except Exception:
+            logging.exception("First message should always be a handshake message")
+            self.healthy = False
+
+        return False
+
+    def _handle_keep_alive(self):
+        try:
+            keep_alive = message.KeepAlive.from_bytes(self.read_buffer)
+            logging.debug('handle_keep_alive - %s' % self.ip)
+        except message.WrongMessageException:
+            return False
+        except Exception:
+            logging.exception("Error KeepALive, (need at least 4 bytes : {})".format(len(self.read_buffer)))
+            return False
+
+        self.read_buffer = self.read_buffer[keep_alive.total_length:]
+        return True
     
+    def get_messages(self):
+        while len(self.read_buffer) > 4 and self.healthy:
+            if (not self.has_handshaked and self._handle_handshake()) or self._handle_keep_alive():
+                continue
+
+            payload_length, = struct.unpack(">I", self.read_buffer[:4])
+            total_length = payload_length + 4
+
+            if len(self.read_buffer) < total_length:
+                break
+            else:
+                payload = self.read_buffer[:total_length]
+                self.read_buffer = self.read_buffer[total_length:]
+
+            try:
+                received_message = message.MessageDispatcher(payload).dispatch()
+                if received_message:
+                    yield received_message
+            except message.WrongMessageException as e:
+                logging.exception(e.__str__())
